22732084bda287d42429a41978c81ed06d4d749b
elokfaiqohh <elokfaiqoh885@gmail.com>
2024-02-15 10:03:12 +0700

    finish rumus

--- js/script.js
+++ js/script.js
@@ -1,17 +1,39 @@
+// function convert() {
+//    ketika tombol konversi di klik akan menjalankan fungsi ini
+//     let celciusIsi = document.getElementById("celcius").value
+//     let fahrenheitIsi = document.getElementById("fahrenheit").value
+//     let celcius = document.getElementById("celcius")
+//     let fahrenheit = document.getElementById("fahrenheit")
+
+//     fahrenheit.innerHTML = (9/5) * celciusIsi + 32
+//     fahrenheit.innerText = fahrenheitIsi
+//     celciusIsi = (fahrenheitIsi - 32) * 5/9
+//     celcius.innerText = celciusIsi
+//     console.log(fahrenheitIsi, celciusIsi)
+//   }
+
 
 function convert() {
-    // ketika tombol konversi di klik akan menjalankan fungsi ini
-    celciusIsi = document.getElementById("celcius").value
-    fahrenheitIsi = document.getElementById("fahrenheit").value
-    celcius = document.getElementById("celcius")
-    fahrenheit = document.getElementById("fahrenheit")
-    cara = document.getElementById("cara")
+  let input = document.getElementById("input").value
+  let hasil = document.getElementById('hasil')
+  let rumus = document.getElementById('rumus')
+  let selected = document.getElementById('select')
+  let valueSelect = selected.options[selected.selectedIndex].value
 
-    fahrenheitIsi = (9/5) * celciusIsi + 32
-    fahrenheit.innerHTML = fahrenheitIsi
-
+  if (valueSelect == 'celcius') {
+        let fahrenheit = ((input * 9 / 5) + 32).toFixed(1)
+        hasil.innerHTML = fahrenheit
+        rumus.innerHTML = fahrenheit +" = (" + input + " * 9 / 5) + 32)"
+  }else{
+        let celcius = ((input - 32) * 5 / 9).toFixed(1)
+        hasil.innerHTML = celcius
+        rumus.innerHTML = celcius +" = (" + input + " - 32) * 5 / 9"
   }
-
+}
   function reset(){
     window.location.reload();
   } 
+
+
+
+  
