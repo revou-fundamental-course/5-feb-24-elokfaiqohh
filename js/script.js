c890128e3e258792a42262a67985468ad1ed61c7
elokfaiqohh <elokfaiqoh885@gmail.com>
2024-02-15 21:06:20 +0700

    finish push

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-// function convert() {
-//    ketika tombol konversi di klik akan menjalankan fungsi ini
-//     let celciusIsi = document.getElementById("celcius").value
-//     let fahrenheitIsi = document.getElementById("fahrenheit").value
-//     let celcius = document.getElementById("celcius")
-//     let fahrenheit = document.getElementById("fahrenheit")
-
-//     fahrenheit.innerHTML = (9/5) * celciusIsi + 32
-//     fahrenheit.innerText = fahrenheitIsi
-//     celciusIsi = (fahrenheitIsi - 32) * 5/9
-//     celcius.innerText = celciusIsi
-//     console.log(fahrenheitIsi, celciusIsi)
-//   }
-
-
 function convert() {
   let input = document.getElementById("input").value
   let hasil = document.getElementById('hasil')
@@ -20,19 +5,38 @@ function convert() {
   let selected = document.getElementById('select')
   let valueSelect = selected.options[selected.selectedIndex].value
 
-  if (valueSelect == 'celcius') {
-        let fahrenheit = ((input * 9 / 5) + 32).toFixed(1)
-        hasil.innerHTML = fahrenheit
-        rumus.innerHTML = fahrenheit +" = (" + input + " * 9 / 5) + 32)"
-  }else{
-        let celcius = ((input - 32) * 5 / 9).toFixed(1)
-        hasil.innerHTML = celcius
-        rumus.innerHTML = celcius +" = (" + input + " - 32) * 5 / 9"
+  if (input != 0 ) {
+    if (valueSelect == 'celcius') {
+          let fahrenheit = ((input * 9 / 5) + 32).toFixed(1)
+          hasil.innerHTML = fahrenheit
+          rumus.innerHTML = fahrenheit +" = (" + input + " * 9 / 5) + 32)"
+    }else{
+          let celcius = ((input - 32) * 5 / 9).toFixed(1)
+          hasil.innerHTML = celcius
+          rumus.innerHTML = celcius +" = (" + input + " - 32) * 5 / 9"
+    }
+  }else {
+    alert('masukkan suhu yang ingin di konvert')
   }
 }
-  function reset(){
-    window.location.reload();
-  } 
+
+
+function reset(){
+  window.location.reload();
+}
+
+function reverse(){
+  let input = document.getElementById("input").value
+  let hasil = document.getElementById('hasil')
+  let rumus = document.getElementById('rumus')
+  let selected = document.getElementById('select')
+  let valueSelect = selected.options[selected.selectedIndex].value
+  var temp = input
+  input.innerHTML = hasil
+  hasil.innerHTML = temp
+
+  
+}
 
 
 
